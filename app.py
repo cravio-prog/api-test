5802d7f223710d1ea508b541083cae74e4bc937c
cravio-prog <andrea.craviotto@gmail.com>
2022-10-18 14:53:48 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template,request,redirect,url_for
+import json
+
+app = Flask(__name__)
+
+@app.route("/api/<nome>",methods=["GET"])
+def apiproc(nome=None):
+    try:
+        if request.method=="POST":
+            return "POST API NOT DEFINED"
+        elif request.method=="GET":
+            print("richiesta POST")
+            print("nome",nome)
+            if nome== "mailfeed":
+                email_get=request.args.get('email', '')
+#                return render_template('output.html', columns=columns, row_data=email_serach[0], zip=zip)
+                return render_template("mailfeed.html",email=email_get)
+            if nome== "article":
+                email_get=request.args.get('email', '')
+#                return render_template('output.html', columns=columns, row_data=email_serach[0], zip=zip)
+                return render_template("article.html",email=email_get)
+
+    except Exception as e:
+            print(e)
+            return "ERR - " + str(e)
+
+@app.route('/')
+def index():
+    return "<p>Hello, World!</p>"
+
+
+'''    
+@app.route("/home")
+@auth.login_required
+def home():
+    return render_template("home.html")
+'''
+
+if __name__ == '__main__':
+    print("__main__")
+    app.run()
